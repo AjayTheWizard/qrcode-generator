3153bb1e2a364b92ed86ef9b27a1682311de6dda
AjayTheWizard <jesualbert51@gmail.com>
2023-10-14 20:12:38 +0530

    feat: theme switch

--- components/UserNavigation.tsx
+++ components/UserNavigation.tsx
@@ -2,8 +2,9 @@
 
 import { useUser } from '@clerk/nextjs'
 import { Button } from '@components/ui/button'
-import Link from 'next/link'
+import Link from 'next/link'                    
 import { UserButton } from '@clerk/nextjs'
+import { BsFillMoonStarsFill } from "react-icons/bs"
 
 export const UserNavigation = () => {
   const { isSignedIn } = useUser()
@@ -16,13 +17,25 @@ export const UserNavigation = () => {
       ) : (
         <>
           <li>
-            <Button asChild>
+            <Button variant="secondary" asChild>
               <Link href="/sign-in">Sign In</Link>
             </Button>
           </li>
           <li>
-            <Button asChild>
+            <Button variant="secondary" asChild>
               <Link href="/sign-up">Sign Up</Link>
+            </Button>
+          </li>
+          <li>
+            <Button size="icon" onClick={()=>{
+              let cls = (document.getElementById("body") as HTMLDivElement).classList;
+              if(cls.contains("dark")){
+                cls.remove("dark")
+              }else {
+                cls.add("dark")
+              }
+            }}>
+              <BsFillMoonStarsFill/>
             </Button>
           </li>
         </>
